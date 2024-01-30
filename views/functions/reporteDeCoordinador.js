3c152809a235f8daf3009fb625317c6d9570ed00
Josue Rojas <jrojas@maxuhle.edu.pe>
2024-01-30 14:49:35 -0500

    coordinator report almost complete

---
+++ views/functions/reporteDeCoordinador.js
@@ -0,0 +1,7 @@
+const rerportSelect = document.getElementById("report-type");
+const registerDate = document.getElementById("teachers-date");
+const assistanceDate = document.getElementById("assistance-date");
+
+console.log(registerDate);
+console.log(rerportSelect);
+console.log(assistanceDate);
